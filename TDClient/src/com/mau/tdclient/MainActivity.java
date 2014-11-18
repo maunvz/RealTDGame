3675345ab90e1889bec6c39a0ed8a9911e802b68
Mauricio Narvaez <maunvz@gmail.com>
2014-11-18 13:57:28 -0800

    Ignored some files

--- TDClient/src/com/mau/tdclient/MainActivity.java
+++ TDClient/src/com/mau/tdclient/MainActivity.java
@@ -12,7 +12,6 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
